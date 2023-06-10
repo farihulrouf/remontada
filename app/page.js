2d806456a4f971819a323337491e626093979d78
Farihul Rouf <farihulrouf@gmail.com>
2023-06-10 16:56:50 +0700

    add footer

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
 export default function Home() {
   return (
+    <>
     <NavBar />
+    <Footer />
+    </>
   )
 }
---
+++ components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react"
+const Footer = () => {
+    return (
+        <React.Fragment>
+            <div className="w-full bg-teal-500">
+                <div className="max-w-screen-xl mx-auto bg-pink-500">
+                    <h2 className="text-white">
+                        <div className="w-full flex">
+                            <div className="w-1/3">
+                                    About
+                            </div>
+                            <div className="w-1/3">
+                                    Link
+                            </div>
+                            <div className="w-1/3">
+                                    About
+                            </div>
+                        </div>
+                    </h2>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+
+}
+export default Footer
