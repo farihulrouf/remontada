4e26102cf57364844a5e82a1abbfc998a3c49367
Farihul Rouf <farihulrouf@gmail.com>
2023-06-10 21:33:53 +0700

    add match

--- components/TabMatch.js
+++ components/TabMatch.js
@@ -1,55 +1,58 @@
 import Image from "next/image"
-
-import { AiOutlineArrowRight } 
-from 'react-icons/ai';
+import { pagematch } from "./data";
+import { AiOutlineArrowRight }
+    from 'react-icons/ai';
 const TabMatch = () => {
     return (
         <div className="w-full mt-8 mb-6 px-24">
-            <div className="flex p-1">
-                <div className="w-1/2">
-                    <div className="flex space-x-2 justify-between px-6">
-                        <div className="flex space-x-2">
-                            <Image
-                                src="/assets/flag/argentina.png"
-                                width={36}
-                                height={36}
-                                alt="Argentina"
-                            />
-                           <p> Argentina</p>
-                        </div>
-                        <div>
-                          <div className="bg-indigo-100 px-2 rounded-xl">
-                            <p className="text-blue-500">1-2</p>
-                          </div>
-                        </div>
-                        <div className="flex space-x-2">
+            {pagematch.map((match, index) =>
+                <>
+                    <div className="flex p-2">
+                        <div className="w-1/2">
+                            <div className="flex space-x-2 justify-between px-6">
+                                <div className="flex space-x-2">
+                                    <Image
+                                        src="/assets/flag/argentina.png"
+                                        width={36}
+                                        height={36}
+                                        alt="Argentina"
+                                    />
+                                    <p> Argentina</p>
+                                </div>
+                                <div>
+                                    <div className="bg-indigo-100 px-2 rounded-xl">
+                                        <p className="text-blue-500">1-2</p>
+                                    </div>
+                                </div>
+                                <div className="flex space-x-2">
 
-                           <p> Brazil</p>
-                            <Image
-                                src="/assets/flag/brazil.png"
-                                width={36}
-                                height={36}
-                                alt="brazil"
-                            />
+                                    <p> Brazil</p>
+                                    <Image
+                                        src="/assets/flag/brazil.png"
+                                        width={36}
+                                        height={36}
+                                        alt="brazil"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2">
+                            <div className="w-full flex justify-between">
+                                <div className="px-2 bg-pink-100 w-28 rounded-xl">
+                                    <p className="text-red-500 text-center">Full - Time</p>
+                                </div>
+                                <div className="flex space-x-4 text-sm">
+                                    <p className="text-gray-400">18 December 2023</p>
+                                    <p className="text-gray-400">Alianz Arena </p>
+                                    <p className="text-gray-400">World Cup U-20 </p>
+                                    <AiOutlineArrowRight size={24} className="text-gray-400" />
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="w-1/2">
-                        <div className="w-full flex justify-between">
-                            <div className="px-2 bg-pink-100 w-28 rounded-xl">
-                                <p className="text-red-500 text-center">Full - Time</p>
-                            </div>
-                            <div className="flex space-x-4 text-sm">
-                                <p className="text-gray-400">18 December 2023</p>
-                                <p className="text-gray-400">Alianz Arena </p>
-                                <p className="text-gray-400">World Cup U-20 </p>
-                                <AiOutlineArrowRight size={24} className="text-gray-400" />
-                            </div>
-                        </div>
-                </div>
-            </div>
-          
-            
+                </>
+            )}
+
         </div>
     )
 }
---
+++ components/data.js
@@ -0,0 +1,64 @@
+
+const pagematch = [
+    {
+        id: 1,
+        name1: "Argentina",
+        name2: "Brazil",
+        dateTime: "18 December 2023",
+        stadium: "Alianz Arena",
+        cup: "World Cup U-20",
+        img: "/img/creator/angie.jpg",
+        score: "1-2"
+    },
+    {
+        id: 2,
+        name1: "Argentina",
+        name2: "Brazil",
+        dateTime: "18 December 2023",
+        stadium: "Alianz Arena",
+        cup: "World Cup U-20",
+        img: "/img/creator/angie.jpg",
+        score: "1-2"
+    },
+
+    {
+        id: 3,
+        name1: "Argentina",
+        name2: "Brazil",
+        dateTime: "18 December 2023",
+        stadium: "Alianz Arena",
+        cup: "World Cup U-20",
+        img: "/img/creator/angie.jpg",
+        score: "1-2"
+    },
+
+    {
+        id: 4,
+        name1: "Argentina",
+        name2: "Brazil",
+        dateTime: "18 December 2023",
+        stadium: "Alianz Arena",
+        cup: "World Cup U-20",
+        img: "/img/creator/angie.jpg",
+        score: "1-2"
+    },
+
+    {
+        id: 5,
+        name1: "Argentina",
+        name2: "Brazil",
+        dateTime: "18 December 2023",
+        stadium: "Alianz Arena",
+        cup: "World Cup U-20",
+        img: "/img/creator/angie.jpg",
+        score: "1-2"
+    },
+    
+
+]
+
+
+
+  export {
+    pagematch
+ }
