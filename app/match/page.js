3ad72b4fb1af401c85290f196c8761c522b1ef19
Farihul Rouf <farihulrouf@gmail.com>
2023-06-12 06:04:32 +0700

    rf match

--- app/match/page.js
+++ app/match/page.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { pagematch } from "@/components/data";
-import { AiOutlineArrowRight }
+import { AiOutlineArrowRight, AiOutlineClockCircle }
     from 'react-icons/ai';
+import { MdOutlineStadium } from 'react-icons/md'
+import { BsArrowRight } from 'react-icons/bs'
+import { BiFootball } from 'react-icons/bi'
 import Image from "next/image"
 const MatchSoccer = () => {
     return (
@@ -17,55 +20,70 @@ const MatchSoccer = () => {
                 />
             </div>
             <div className="max-w-screen-xl mx-auto px-24 py-8">
-                <div className="bg-yellow-500 p-2 mb-8">
+                <div className="bg-yellow-500 p-2 mb-8 flex items-center">
+                    <BiFootball size={36} />
                     <h2 className="text-lg">Footbal match</h2>
                 </div>
                 {pagematch.map((match, index) =>
-                <>
-                    <div className="flex p-4">
-                        <div className="w-1/2">
-                            <div className="flex space-x-2 justify-between">
-                                <div className="flex space-x-2">
-                                    <Image
-                                        src={match.img_1}
-                                        width={36}
-                                        height={36}
-                                        alt="Argentina"
-                                    />
-                                    <p className="w-36 text-left">{match.name1}</p>
-                                </div>
-                                <div>
-                                    <div className="bg-indigo-100 px-2 rounded-xl">
-                                        <p className="text-blue-500">{match.score}</p>
+                    <>
+                        <div className="flex space-x-8 p-4 items-center">
+                            <div className="w-2/5">
+                                <div className="flex space-x-2 justify-between">
+                                    <div className="flex space-x-2 justify-start w-36">
+                                        <Image
+                                            src={match.img_1}
+                                            width={36}
+                                            height={36}
+                                            alt="Argentina"
+                                        />
+                                        <p className="text-left">{match.name1}</p>
+                                    </div>
+                                    <div className="">
+                                        <div className="bg-indigo-100 px-2 rounded-xl">
+                                            <p className="text-blue-500">{match.score}</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex space-x-2 w-36 justify-end">
+                                        <p className="text-right"> {match.name2}</p>
+                                        <Image
+                                            src={match.img_2}
+                                            width={36}
+                                            height={36}
+                                            alt="brazil"
+                                        />
                                     </div>
                                 </div>
-                                <div className="flex space-x-2">
-                                    <p className="w-36 text-right"> {match.name2}</p>
+                            </div>
+                            <div className="w-3/5 flex space-x-8 items-center">
+                                <div className="flex items-center space-x-2">
+                                    <input type="text" placeholder="-" className="border w-12 rounded-lg" />
+                                    <p className="text-[12px] text-gray-500">Prediction</p>
+                                    <input type="text" placeholder="-" className="border w-12 rounded-lg" />
+                                </div>
+                                <div className="flex items-center space-x-2">
+                                    <AiOutlineClockCircle size={32} />
+                                    <p className="text-gray-700">5.00 PM</p>
+                                </div>
+                                <div className="flex items-center space-x-2">
+                                    <MdOutlineStadium size={32} />
+                                    <p className="text-gray-700">London</p>
+                                </div>
+                                <div className="flex items-center space-x-2">
                                     <Image
-                                        src={match.img_2}
-                                        width={36}
-                                        height={36}
-                                        alt="brazil"
+                                        src="/assets/icon/cup.png"
+                                        width={38}
+                                        height={54}
+                                        alt="World Cup"
                                     />
                                 </div>
-                            </div>
-                        </div>
-                        <div className="w-1/2">
-                            <div className="w-full flex justify-between">
-                                <div className="px-2 bg-pink-100 w-28 rounded-xl ml-4">
-                                    <p className="text-red-500 text-center">Full - Time</p>
-                                </div>
-                                <div className="flex space-x-4 text-sm">
-                                    <p className="text-gray-400">18 December 2023</p>
-                                    <p className="text-gray-400">Alianz Arena </p>
-                                    <p className="text-gray-400">World Cup U-20 </p>
-                                    <AiOutlineArrowRight size={24} className="text-gray-400" />
+                                <div className="flex items-center space-x-2">
+                                    <p className="text-gray-700">Details</p>
+                                    <BsArrowRight size={32} />
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </>
-            )}
+                    </>
+                )}
             </div>
         </React.Fragment>
     )
