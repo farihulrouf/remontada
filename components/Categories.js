3595a21ff54034d3302c4194bf25a5c653b46664
Farihul Rouf <farihulrouf@gmail.com>
2023-06-13 07:32:15 +0700

    categoris r

--- components/Categories.js
+++ components/Categories.js
@@ -5,6 +5,9 @@ const Categories = () => {
     return (
         <React.Fragment>
             <div className="relative">
+                <img src="/assets/soccer.png" className="absolute left-0 sm:w-[283px] sm:h-[256px] w-[70px] h-[64px]" alt="soccer" />
+
+                {/*
             <Image
                     className="absolute left-0"
                         src="/assets/soccer.png"
@@ -12,8 +15,21 @@ const Categories = () => {
                         height={256}
                         alt="Picture of the author"
                     />
+    */ }
                 <h2 className="text-4xl text-center">فئات</h2>
-                <div className="flex flex-wrap justify-center w-1/2 mx-auto gap-3 py-2">
+                <div className="flex flex-wrap justify-center sm:w-1/2 w-4/5 mx-auto gap-3 py-2">
+                    <img src="/assets/icon/ucl.png" alt="ucl" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+                    <img src="/assets/icon/uefa.png" alt="uefa" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+                    <img src="/assets/icon/ipl.png" alt="ipl" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+                    <img src="/assets/icon/blg.png" alt="blg" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+                    <img src="/assets/icon/lag.png" alt="lag" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+                    <img src="/assets/icon/seria.png" alt="seria" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+                    <img src="/assets/icon/uero.png" alt="uero" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+                    <img src="/assets/icon/wc.png" alt="wc" className="w-[60px] h-[50px] sm:w-[121px] sm:h-[100px]"/>
+
+
+
+                    {/*
                     <Image
                         src="/assets/icon/ucl.png"
                         width={121}
@@ -62,6 +78,7 @@ const Categories = () => {
                         height={100}
                         alt="Picture of the author"
                     />
+*/}
                 </div>
 
             </div>
