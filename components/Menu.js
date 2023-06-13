ad4c33bdd960f597fe9231fd9d2548059abcd18d
Farihul Rouf <farihulrouf@gmail.com>
2023-06-13 16:56:05 +0700

    menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 const Menu = () => {
-    return 
-    <div className="px-2 md:px-0 py-2 space-y-2 md:space-y-0 md:space-x-2 font-medium text-teal-200 text-sm">
+    return <div className="px-2 md:px-0 py-2 space-y-2 md:space-y-0 md:space-x-2 font-medium text-teal-200 text-sm">
         <a
             
             className="block md:inline-block px-3 py-2 rounded-md text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
@@ -15,7 +14,7 @@ const Menu = () => {
             
             className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
         >
-            <Link href="/about">
+            <Link href="/">
 
                 About
             </Link>
@@ -35,9 +34,7 @@ const Menu = () => {
                 Termn and Condition
             </Link>
         </a>
-        
     </div>
-    
 }
 
 export default Menu;
