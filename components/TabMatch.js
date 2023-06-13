74f17b3dcaff26ebd073e780e1b49306366d96c2
Farihul Rouf <farihulrouf@gmail.com>
2023-06-13 08:44:07 +0700

    match r

--- components/TabMatch.js
+++ components/TabMatch.js
@@ -2,19 +2,22 @@ import Image from "next/image"
 import { pagematch } from "./data";
 import { AiOutlineArrowRight }
     from 'react-icons/ai';
+import { BiFootball } from 'react-icons/bi'
+
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const TabMatch = () => {
     return (
-        <div className="w-full mt-12 mb-24 px-24">
-            <div className="bg-yellow-500 p-2 mb-8">
+        <div className="w-full mt-12 mb-24 sm:px-24 px-4">
+            <div className="bg-yellow-500 p-2 mb-8 flex items-center">
+                <BiFootball size={32} />
                 <h2 className="text-lg">Footbal match</h2>
             </div>
-            
+           
             {pagematch.map((match, index) =>
                 <>
-                    <div className="flex p-4">
-                        <div className="w-1/2">
+                    <div className="flex flex-wrap items-center sm:p-4 p-0">
+                        <div className="sm:w-1/2 w-full">
                             <div className="flex space-x-2 justify-between">
                                 <div className="flex space-x-2">
                                     <Image
@@ -23,7 +26,7 @@ const TabMatch = () => {
                                         height={36}
                                         alt="Argentina"
                                     />
-                                    <p className="w-36 text-left">{match.name1}</p>
+                                    <p className="w-16 sm:w-36 text-left">{match.name1}</p>
                                 </div>
                                 <div>
                                     <div className="bg-indigo-100 px-2 rounded-xl">
@@ -31,7 +34,7 @@ const TabMatch = () => {
                                     </div>
                                 </div>
                                 <div className="flex space-x-2">
-                                    <p className="w-36 text-right"> {match.name2}</p>
+                                    <p className="w-16 sm:w-36 text-right"> {match.name2}</p>
                                     <Image
                                         src={match.img_2}
                                         width={36}
@@ -41,22 +44,25 @@ const TabMatch = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="w-1/2">
-                            <div className="w-full flex justify-between">
-                                <div className="px-2 bg-pink-100 w-28 rounded-xl ml-4">
-                                    <p className="text-red-500 text-center">Full - Time</p>
+                        
+                        <div className="sm:w-1/2 w-full">
+                            <div className="w-full flex items-center justify-between mt-2 mb-2">
+                                <div className="px-2 bg-pink-100 rounded-xl sm:ml-4 ml-0">
+                                    <p className="text-red-500 text-center sm:text-sm text-[10px]">Full - Time</p>
                                 </div>
-                                <div className="flex space-x-4 text-sm">
-                                    <p className="text-gray-400">18 December 2023</p>
-                                    <p className="text-gray-400">Alianz Arena </p>
-                                    <p className="text-gray-400">World Cup U-20 </p>
-                                    <AiOutlineArrowRight size={24} className="text-gray-400" />
+                                <div className="flex items-center sm:space-x-4 space-x-1 sm:text-sm text-[10px] px-2">
+                                    <p className="bg-teal-500 text-white px-2 rounded-xl">18 Dec 2023</p>
+                                    <p className="bg-pink-500 text-white px-2 rounded-xl">Alianz Arena </p>
+                                    <p className="bg-indigo-700 text-white px-2 rounded-xl">World Cup U-20 </p>
+                                    <AiOutlineArrowRight className="text-gray-400" />
                                 </div>
                             </div>
                         </div>
+            
                     </div>
                 </>
             )}
+            
 
         </div>
     )
