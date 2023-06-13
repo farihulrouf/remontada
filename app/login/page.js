cf6bc8abd27cea47b604d0d56a9ba5fe74c3336b
Farihul Rouf <farihulrouf@gmail.com>
2023-06-13 16:58:33 +0700

    placeholder

--- app/login/page.js
+++ app/login/page.js
@@ -44,8 +44,8 @@ const Login = () => {
                     <div className="sm:w-1/2 w-full">
                         <h3 className="text-2xl text-center sm:mt-8 mt-0">ابدأ في تخمين مباريات اليوم</h3>
                         <div className="w-full bg-gray-200 flex flex-col p-24 mt-6 h-72 relative">
-                            <input type="password" className="p-2 px-4 border sm:w-full w-36 mb-2 rounded-xl" placeholder="أدخل كلمة المرور هنا" onChange={matchSoccer} />
-                            <button className="bg-yellow-400 px-2 w-36 py-2 mt-2 mb-2 rounded-xl flex justify-center items-center space-x-3" onClick={matchREdirect}><label>ابدأ التخمين</label> <BsArrowRight size={24} /></button>
+                            <input type="password" className="p-2 px-4 border sm:w-full w-36 mb-2 rounded-xl" placeholder="Password" onChange={matchSoccer} />
+                            <button className="bg-yellow-400 px-2 w-36 py-2 mt-2 mb-2 rounded-xl flex justify-center items-center space-x-3" onClick={matchREdirect}><label>Guess Now</label> <BsArrowRight size={24} /></button>
                             <div className="absolute right-10 bottom-5">
                                 <Image
                                     src="/assets/splash/key 1.png"
