76e1fe0f3f9b4a2f30bfc98f39accf15da90816a
Farihul Rouf <farihulrouf@gmail.com>
2023-06-15 12:06:43 +0700

    add

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -117,6 +117,8 @@ const Dashboard = () => {
     const [open, setOpen] = useState(false)
     const closeModal = () => setOpen(false)
     const [file, setFile] = useState();
+    const [file_2, setFile_2] = useState();
+    const [file_3, setFile_3] = useState();
     //const [value, onChange] = useState('10:00');
     const [startDate, setStartDate] = useState(new Date());
 
@@ -125,7 +127,16 @@ const Dashboard = () => {
         console.log(e.target.files);
         setFile(URL.createObjectURL(e.target.files[0]));
     }
-    
+    const handleChange_uploud = (e) => {
+        console.log(e.target.files);
+        setFile_2(URL.createObjectURL(e.target.files[0]));
+    }
+    const handleChange_stadium = (e) => {
+
+        console.log(e.target.files);
+        setFile_3(URL.createObjectURL(e.target.files[0]));
+    }
+
     return (
         <React.Fragment>
             <div className="container max-w-screen-xl mx-auto">
@@ -223,18 +234,38 @@ const Dashboard = () => {
 
             <Popup open={open} closeOnDocumentClick onClose={closeModal}>
                 <div className='md:w-[678px] w-[350px] bg-white shadow-md flex flex-col p-6 font-play'>
-                    <div className='flex space-x-2 justify-between'>
-                        <input type='text' placeholder='Club 1' className='px-2 border py-1' /> <spam>VS</spam><input type='text' placeholder='Club 2' className='px-2 border py-1' />
+                    <div className='flex space-x-2 py-2'>
+                        <input type='text' placeholder='Club 1' className='px-2 border py-1' /> <input type='text' placeholder='score' className='w-12 py-1 border' />
+                    </div>
+                    <div className='py-2 flex items-center justify-between space-x-2 text-[12px]'>
+
+                        <input type="file" onChange={handleChange_uploud} />
+                        <img className='w-32 h-auto' src={file_2} />
+                    </div>
+                    <div className='flex space-x-2 py-2'>
+                        <input type='text' placeholder='Club 2' className='px-2 border py-1' /> <input type='text' placeholder='score' className='w-12 py-1 border' />
+                    </div>
+
+                    <div className='py-2 flex items-center justify-between space-x-2 text-[12px]'>
+
+                        <input type="file" onChange={handleChange} />
+                        <img className='w-32 h-auto' src={file} />
+                    </div>
+
+                    <div className='py-2'>
+                        <input type='text' placeholder='Name Stadium' className='px-2 py-1 border w-full' />
                     </div>
                     <div className='py-2'>
-                        <input type='text' placeholder='stadium' className='px-2 py-1 border w-full' />
-                    </div>
-                    <div>
                         <input type='text' className='w-full px-2 py-1 border' placeholder='Name of copetition' />
+                        <div className='py-2 text-[12px]'>
+
+                            <input type="file" onChange={handleChange_stadium} />
+                            <img className='w-32 h-auto' src={file_3} />
+                        </div>
                     </div>
                     <div className='py-2 space-x-4 items-center'>
-                             <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className='border px-2 py-1' />
-                                
+                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className='border px-2 py-1' />
+
                         {/* <TimePicker onChange={onChange} value={value} /> */}
 
                     </div>
